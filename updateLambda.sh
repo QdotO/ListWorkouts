358378645e80d042f90a057c57efdf9d25c6a464
Quincy <quincyobeng@gmail.com>
2018-08-13 18:01:33 -0500

    Correcting error in callback error presenting as [Object object]

--- updateLambda.sh
+++ updateLambda.sh
@@ -7,8 +7,8 @@ if [ -e "$file" ]; then
 fi 
 
 
-zip -r listworkouts.zip .
+# zip -r listworkouts.zip .
+zip -r listworkouts.zip . -x *.git*
 echo "All zipped up"
 echo "Now deploying..."
 aws lambda update-function-code --function-name ListWorkouts --zip-file fileb://listworkouts.zip
-echo "Done"
